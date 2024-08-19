a09b3a8d7577abeed8ae582d4751a13f177b0e19
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-08-18 21:25:21 -0600

    :tada: Initial commit

---
+++ src/main/java/com/ita/mx/proyecto_ita/ProyectoItaApplication.java
@@ -0,0 +1,13 @@
+package com.ita.mx.proyecto_ita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoItaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoItaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ita/mx/proyecto_ita/ProyectoItaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ita.mx.proyecto_ita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoItaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
